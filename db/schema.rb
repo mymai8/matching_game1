dde303771c6e7144fca18587817cbaa025fc2270
mymai8 <mywork1868@gmail.com>
2024-02-09 11:38:56 +0900

    ユーザー新規登録

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_09_014817) do
+  create_table "coaches", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "rank_id", null: false
+    t.string "kill_rate", null: false
+    t.text "character", null: false
+    t.string "play_style", null: false
+    t.string "play_time", null: false
+    t.text "play_device", null: false
+    t.text "communication_tool", null: false
+    t.string "price", null: false
+    t.string "times_to_teach", null: false
+    t.integer "rank_limit_id", null: false
+    t.text "appeal_point", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_coaches_on_user_id"
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "game_name", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "coaches", "users"
 end
